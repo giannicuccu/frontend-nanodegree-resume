aa51289bcbfc327409ece2fc65f3aefaa284ed4b
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-06-08 20:36:33 +0200

    Add gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,45 @@
 /*eslint-env node */
 
-var gulp = require('gulp');
+const gulp = require('gulp');
 const babel = require('gulp-babel');
-
-// gulp.task('transpile', () =>
-//     gulp.src('./js/resumeBuilder.js')
-//         .pipe(babel({
-//             presets: ['env']
-//         }))
-//         .pipe(gulp.dest('dist/js'))
-// );
-
-// var sass = require('gulp-sass');
-// var autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 // var browserSync = require('browser-sync').create();
 // var eslint = require('gulp-eslint');
 // var jasmine = require('gulp-jasmine-phantom');
-// var concat = require('gulp-concat');
- var uglify = require('gulp-uglify');
 
-// gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'scripts'], function() {
-// 	gulp.watch('sass/**/*.scss', ['styles']);
-// 	gulp.watch('js/**/*.js', ['lint']);
-// 	gulp.watch('/index.html', ['copy-html']);
-// 	gulp.watch('./dist/index.html').on('change', browserSync.reload);
-
-// 	browserSync.init({
-// 		server: './dist'
-// 	});
-// });
-
-// gulp.task('dist', [
-// 	'copy-html',
-// 	'copy-images',
-// 	'styles',
-// 	'lint',
-// 	'scripts-dist'
-// ]);
-
-// gulp.task('scripts', function() {
-// 	gulp.src('js/**/*.js')
-// 		.pipe(concat('all.js'))
-// 		.pipe(gulp.dest('dist/js'));
-// });
+gulp.task('default', ['scripts-dist', 'copy-html', 'styles'], function() {
+	gulp.watch('css/**/*.css', ['styles']);
+	gulp.watch('js/**/*.js', ['scripts-dist']);
+	gulp.watch('./index.html', ['copy-html']);
+    //gulp.watch('./dist/index.html').on('change', browserSync.reload);
+});
 
 gulp.task('scripts-dist', function() {
-	gulp.src('js/**/*.js')
-		// .pipe(concat('all.js'))
+    gulp.src('js/**/*.js')
+        // .pipe(concat('all.js'))
         .pipe(babel({
             presets: ['env']
         }))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
-// gulp.task('copy-html', function() {
-// 	gulp.src('./index.html')
-// 		.pipe(gulp.dest('./dist'));
-// });
+gulp.task('copy-html', function() {
+    gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+});
 
-// gulp.task('copy-images', function() {
-// 	gulp.src('images/*')
-// 		.pipe(gulp.dest('dist/images'));
-// });
-
-// gulp.task('styles', function() {
-// 	gulp.src('sass/**/*.scss')
-// 		.pipe(sass({
-// 			outputStyle: 'compressed'
-// 		}).on('error', sass.logError))
-// 		.pipe(autoprefixer({
-// 			browsers: ['last 2 versions']
-// 		}))
-// 		.pipe(gulp.dest('dist/css'))
-// 		.pipe(browserSync.stream());
-// });
-
-// gulp.task('lint', function () {
-// 	return gulp.src(['js/**/*.js'])
-// 		// eslint() attaches the lint output to the eslint property
-// 		// of the file object so it can be used by other modules.
-// 		.pipe(eslint())
-// 		// eslint.format() outputs the lint results to the console.
-// 		// Alternatively use eslint.formatEach() (see Docs).
-// 		.pipe(eslint.format())
-// 		// To have the process exit with an error code (1) on
-// 		// lint error, return the stream and pipe to failOnError last.
-// 		.pipe(eslint.failOnError());
-// });
-
-// gulp.task('tests', function () {
-// 	gulp.src('tests/spec/extraSpec.js')
-// 		.pipe(jasmine({
-// 			integration: true,
-// 			vendor: 'js/**/*.js'
-// 		}));
-// });
+gulp.task('styles', function() {
+    gulp.src('css/**/*.css')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('dist/css'))
+        //.pipe(browserSync.stream());
+});
